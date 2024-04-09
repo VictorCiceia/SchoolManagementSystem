79cdfd8a13fee21ffbed1ff79f02565efb8c81b1
VictorCiceia <victormciceia@gmail.com>
2024-04-08 23:32:58 -0400

    Herencia con la Clase Persona

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,48 +1,8 @@
 import java.util.Date;
 
-public class Estudiante {
-
-    private long id;
-
-    private String nombre;
-
-    private String apellido;
-
-    private Date fechaDeNacimiento;
+public class Estudiante extends Persona {
 
     private String estado;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public Date getFechaDeNacimiento() {
-        return fechaDeNacimiento;
-    }
-
-    public void setFechaDeNacimiento(Date fechaDeNacimiento) {
-        this.fechaDeNacimiento = fechaDeNacimiento;
-    }
 
     public String getEstado() {
         return estado;
